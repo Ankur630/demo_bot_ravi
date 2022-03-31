fa58fa9490219b65c65a8a7ff50786c845449c1b
Ankur630 <100507265+Ankur630@users.noreply.github.com>
2022-03-31 22:03:32 +0530

    Update core.py

--- core.py
+++ core.py
@@ -2,7 +2,7 @@
 bot_token = "5101895473:AAHvSIEFxW7MLmOG8Op4RdQYYjRqRp1FIjk"
 
 #Admins Telegram ID
-admins = [1468386562,1914289455]
+admins = [1056351854,1056351854]
 
 #MongoDb Url For Databse
 mongo_url = "mongodb+srv://Anujsin:<password>@cluster0.ehi4u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
